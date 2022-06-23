3cf04a0ec9c9f67763faf0a3ce6c342a80d7eb52
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-23 19:22:02 -0300

    feat: finish userLogin method at controller layer

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 
 import UserService from '../service/userService';
 
+import { generateToken } from '../auth';
+
 export default class UserController {
   private _service;
 
@@ -26,5 +28,13 @@ export default class UserController {
     req: Request,
     res: Response,
     next: NextFunction,
-  ): Promise<Response | void> {}
+  ): Promise<Response | void> {
+    try {
+      await this._service.userLogin(req.body);
+      const token = generateToken(req.body.email);
+      return res.status(200).json({ token });
+    } catch (error) {
+      return next(error);
+    }
+  }
 }
