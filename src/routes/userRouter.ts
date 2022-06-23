26c123d972c1c10eec02b7061758910efbed992d
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-23 19:27:39 -0300

    feat: implement login router at userRouter.ts

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -2,14 +2,21 @@ import { Router } from 'express';
 
 import UserController from '../controller/userController';
 import RegisterValidation from '../middleware/registerValidation';
+import LoginValidation from '../middleware/loginValidation';
 
 const router = Router();
 const controller = new UserController();
-const middleware = new RegisterValidation();
+const registerValidation = new RegisterValidation();
+const loginValidation = new LoginValidation();
 
 router
   .post('/register',
-  middleware.validate,
+  registerValidation.validate,
   (req, res, next) => controller.registerUser(req, res, next));
+
+router
+  .post('/login',
+  loginValidation.validate,
+  (req, res, next) => controller.userLogin(req, res, next));
 
 export default router;
