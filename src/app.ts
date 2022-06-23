0622db8b852c2464dd4b55055d77d99e3cb696d7
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-23 16:40:18 -0300

    feat: implements class App at app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,36 @@
-// code
+import * as express from 'express';
+
+export default class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+
+  private config(): void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      return next();
+    }
+
+    this.app.use(accessControl);
+  }
+
+  public start(PORT: string | number): void {
+    this.app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  }
+
+  private routes(): void {
+    this.app.use(express.json());
+
+    this.app.get('/', async (_req, res) => {
+      return res.status(200).json({ message: `Everything's ok!` });
+    });
+  }
+}
