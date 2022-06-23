a5351c51596635728cf9312c9dcddcb81b6dd923
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-23 18:47:27 -0300

    feat: create userRouter

---
+++ src/routes/userRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+import UserController from '../controller/userController';
+import RegisterValidation from '../middleware/registerValidation';
+
+const router = Router();
+const controller = new UserController();
+const middleware = new RegisterValidation();
+
+router
+  .post('/', middleware.validate, (req, res) => controller.registerUser(req, res));
+
+export default router;
