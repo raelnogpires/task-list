ed9e421e23c76233144f068d9443726716db6eca
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-23 19:20:16 -0300

    feat: implement verifyToken and generateToken functions for jwt

---
+++ src/auth/index.ts
@@ -0,0 +1,25 @@
+import * as jwt from 'jsonwebtoken';
+
+import jwtConfig from './config';
+
+type TokenValidateData = {
+  data: { email: string },
+}
+
+export const generateToken = (email: string): string => {
+  const { secret, expiresIn, algorithm } = jwtConfig;
+
+  const token = jwt.sign({ email }, secret, { expiresIn, algorithm });
+
+  return token;
+};
+
+export const verifyToken = (token: string): TokenValidateData | null => {
+  const { secret } = jwtConfig;
+  // try/catch preventing 'jsonwebtoken mal formed' type of error
+  try {
+    return jwt.verify(token, secret) as TokenValidateData;
+  } catch (error) {
+    return null;
+  }
+}
