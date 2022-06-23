9fc7a64eb161bf8c9dfeafa84657e6491c13e37e
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-23 18:43:14 -0300

    feat: create registerValidation middleware

---
+++ src/middleware/registerValidation.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from 'express';
+
+import * as Joi from 'joi';
+
+import { BadRequestError } from 'restify-errors';
+
+import User from '../database/models/User';
+
+const emailRegex = /\S+@\S+\.\S+/;
+const validateEmailWithRegex = (email: string) => emailRegex.test(email);
+
+export default class RegisterValidation {
+  private _model;
+
+  constructor() {
+    this._model = User;
+  }
+
+  private static joi = Joi.object({
+    username: Joi.string().min(4).required(),
+    email: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+  });
+
+  public async validate(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    const { error } = RegisterValidation.joi.validate(req.body);
+    if (error) {
+      const err = new BadRequestError(error.message);
+      return next(err);
+    }
+
+    const emailFormatValidation = validateEmailWithRegex(req.body.email)
+    if (!emailFormatValidation) {
+      const err = new BadRequestError('"email" must be a valid email');
+      return next(err);
+    }
+
+    const userExist = await this._model.findOne({ where: { email: req.body.email } })
+    if (userExist) {
+      const err = new BadRequestError('Email already registered');
+      return next(err);
+    }
+
+    return next();
+  }
+}
