0ffd27636556a04efcfdc70eab2a38b90aab49c3
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-23 16:34:36 -0300

    refactor: now passwords are hashed before entering db

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,5 +1,7 @@
 import User from '../database/models/User';
 
+import { hashPassword } from '../utils/encrypting';
+
 import IUserService from '../@types/interfaces/userService.interface';
 import { CreateUserT } from '../@types/types/user.type';
 
@@ -11,6 +13,8 @@ export default class UserService implements IUserService {
   }
 
   public async registerUser(user: CreateUserT): Promise<void> {
-    await this._model.create(user);
+    const { username, password, email } = user;
+    const hash = await hashPassword(password)
+    await this._model.create({ username, password: hash, email });
   }
 }
